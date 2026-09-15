b564f72bf6b339609ef73896dc91b0be2ad1405e
If a Sundae exists then a Wenzdae also exists! <paragas.wp@obmontessori.edu.ph>
2026-09-15 08:18:11 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+from pyscript import display
+
+firstname = "Wenzelvic" #str
+lastname = "Paragas" #str
+display(firstname)
+display(lastname)
+
+age = 14 #int
+display(age)
+
+height1 = "5'7" #str
+display(height1)
+
+countries = ["South Korea"
+, "Canada", "Malaysia"] #list
+
+display(countries)
+
+student_type = False #bool
+display(student_type)
+
+about_me = {
+    'color':'Violet',
+    'car_Brand':'Honda',
+    'shoe-size': 8.5,
+    'best_friend':'Jeremiah Velasco'
+    } #dict
+display(about_me)
+
+favfruits = set(['pineapples', 'blueberries', 'raspberries']) #set
+display(favfruits)
+
+daysofweek = ('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday') #tuple
+display(daysofweek)
+
